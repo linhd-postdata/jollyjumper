94dc932b0ba15a278a85bfb65b642e3232e069fb
alvp <alvaro@Blue.localdomain>
2019-06-28 11:43:52 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def read(*names, **kwargs):
 
 setup(
     name='jollyjumper',
-    version='0.2.0',
+    version='0.3.0',
     license='Apache Software License 2.0',
     description='Enjambment tool for Spanish texts',
     long_description='%s\n%s' % (
